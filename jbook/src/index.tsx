618edda2509e55b44ff8db29d32da74af4d32991
Ben Bowen <bbowen@bhgpdi.net>
2021-07-04 23:00:44 -0400

    Add files for jBook demo app

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,24 +1,40 @@
 import ReactDOM from 'react-dom';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-    console.log(service);
   };
   useEffect(() => {
     startService();
   }, []);
 
-  const onClick = () => {
-    console.log(input);
+  const onClick = async () => {
+    if (!ref.current) {
+      console.log('noref');
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
