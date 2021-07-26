844719c1018f313ebccbadd899c498b06baf7e5d
Ben Bowen <bbowen@bhgpdi.net>
2021-07-25 20:34:10 -0400

    WIP: Added vertical resizer and started horizontal resizer

--- jbook/src/components/preview.tsx
+++ jbook/src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import React, { useEffect, useRef } from 'react';
 
 interface PreviewProps {
@@ -34,12 +35,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      title="code preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="code preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
