38752780f9ecfb98213aa456e5d7c51baf348398
Ben Bowen <bbowen@bhgpdi.net>
2021-07-31 18:06:52 -0400

    WIP: start implementing state with redux

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 //import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
